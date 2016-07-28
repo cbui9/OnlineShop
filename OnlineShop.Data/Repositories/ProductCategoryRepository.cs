c60d4fd6033d76fb280c4b7ebfa53fd0fd95c070
cbui9 <documentationangularmvc@gmail.com>
2016-07-28 20:52:39 +0700

    update

--- OnlineShop.Data/Repositories/ProductCategoryRepository.cs
+++ OnlineShop.Data/Repositories/ProductCategoryRepository.cs
@@ -12,11 +12,9 @@ namespace OnlineShop.Data.Repositories
 
     public class ProductCategoryRepository : RepositoryBase<ProductCategory>, IProductCategoryRepository
     {
-        public ProductCategoryRepository(IDbFactory dbFactory)
-            : base(dbFactory)
+        public ProductCategoryRepository(IDbFactory dbFactory): base(dbFactory)
         {
         }
-
         public IEnumerable<ProductCategory> GetByAlias(string alias)
         {
             return this.DbContext.ProductCategories.Where(x => x.Alias == alias);
