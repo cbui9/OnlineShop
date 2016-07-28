480dcef653b8dbcaa1f90ad31e67498d8348017b
cbui9 <documentationangularmvc@gmail.com>
2016-07-28 21:02:15 +0700

    update ProductCategoryRepo and ProductRepo

--- OnlineShop.Data/Repositories/ProductCategoryRepository.cs
+++ OnlineShop.Data/Repositories/ProductCategoryRepository.cs
@@ -1,10 +1,7 @@
 ﻿using OnlineShop.Data.Infrastructure;
 using OnlineShop.Model.Models;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace OnlineShop.Data.Repositories
 {
@@ -15,8 +12,7 @@ namespace OnlineShop.Data.Repositories
 
     public class ProductCategoryRepository : RepositoryBase<ProductCategory>, IProductCategoryRepository
     {
-        public ProductCategoryRepository(IDbFactory dbFactory)
-            : base(dbFactory)
+        public ProductCategoryRepository(IDbFactory dbFactory) : base(dbFactory)
         {
         }
 
