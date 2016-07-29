a608b3fde2046992e3c37ef03d8c96f4bcb8407b
cbui9 <documentationangularmvc@gmail.com>
2016-07-29 21:01:14 +0700

    fix bugs for repository and add service

--- OnlineShop.Model/Models/Post.cs
+++ OnlineShop.Model/Models/Post.cs
@@ -1,4 +1,6 @@
 ﻿using OnlineShop.Model.Abstract;
+using System.Collections;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -37,5 +39,8 @@ namespace OnlineShop.Model.Models
 
         [ForeignKey("CategoryID")]
         public virtual PostCategory PostCategory { set; get; }
+
+        public virtual IEnumerable<PostTag> PostTags { set; get; }
+
     }
 }
