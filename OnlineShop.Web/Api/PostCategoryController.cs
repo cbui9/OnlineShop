768f42d306aa60d970b055f1460b19e4e6a9efd6
cbui9 <documentationangularmvc@gmail.com>
2016-08-02 00:17:18 +0700

    add dependency injection autofac to web

--- OnlineShop.Web/Api/PostCategoryController.cs
+++ OnlineShop.Web/Api/PostCategoryController.cs
@@ -80,17 +80,8 @@ namespace OnlineShop.Web.Api
         {
             return CreateHttpResponse(request, () =>
             {
-                HttpResponseMessage response = null;
-                if (ModelState.IsValid)
-                {
-                    request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
-                }
-                else
-                {
-                    var listCategory =  _postCategoryService.GetAll();
-                    _postCategoryService.SaveChanges();
-                    response = request.CreateResponse(HttpStatusCode.OK,listCategory);
-                }
+                var listCategory =  _postCategoryService.GetAll();
+                HttpResponseMessage response = request.CreateResponse(HttpStatusCode.OK,listCategory);
                 return response;
             });
         }
