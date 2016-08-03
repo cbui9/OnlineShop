904006deed2d8f16228fb827bb8ad4c9ad3d06ec
cbui9 <documentationangularmvc@gmail.com>
2016-08-03 16:01:06 +0700

    scrope and rootScope in angularjs

---
+++ OnlineShop.Web/Scripts/spa/app.js
@@ -0,0 +1,19 @@
+﻿/// <reference path="../plugins/angular/angular.js" />
+var myapp = angular.module('myModule', []);
+
+myapp.controller('schoolController', schoolController);
+myapp.controller('studentController', studentController);
+myapp.controller('teacherController', teacherController);
+
+
+//declare
+function schoolController($scope) {
+    $scope.message = "This is message from School";
+}
+
+function studentController($scope){
+    $scope.message = "This is message from Student";
+}
+function teacherController($scope) {
+    $scope.message = "This is message from Teacher";
+}
