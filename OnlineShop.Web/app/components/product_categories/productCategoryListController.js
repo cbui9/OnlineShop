144cda33536324a0dfbc3dbdc4445d51032534ba
cbui9 <documentationangularmvc@gmail.com>
2016-08-09 11:18:00 +0700

    admin: binding product category and filter (angularjs)

---
+++ OnlineShop.Web/app/components/product_categories/productCategoryListController.js
@@ -0,0 +1,19 @@
+﻿/// <reference path="/Assets/admin/libs/angular/angular.js" />
+(function (app) {
+    app.controller('productCategoryListController', productCategoryListController);
+    productCategoryListController.$inject = ['$scope', 'apiService']
+
+    function productCategoryListController($scope, apiService) {
+        $scope.productCategories = [];
+
+        $scope.getProductCategories = getProductCategories;
+        function getProductCategories() {
+            apiService.get('api/productcategory/getall', null, function (result) {
+                $scope.productCategories = result.data;
+            }, function () {
+                console.log('Load product category failed!');
+            });
+        }
+        $scope.getProductCategories();
+    }
+})(angular.module('onlineshop.product_categories'));
