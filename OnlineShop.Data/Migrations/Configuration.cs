2c64764d98142ebfc24c5db29f6c2b67d7d138dc
cbui9 <documentationangularmvc@gmail.com>
2016-08-09 10:24:37 +0700

    admin: binding product category

--- OnlineShop.Data/Migrations/Configuration.cs
+++ OnlineShop.Data/Migrations/Configuration.cs
@@ -1,9 +1,10 @@
-namespace OnlineShop.Data.Migrations
+﻿namespace OnlineShop.Data.Migrations
 {
     using Microsoft.AspNet.Identity;
     using Microsoft.AspNet.Identity.EntityFramework;
     using Model.Models;
     using System;
+    using System.Collections.Generic;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
@@ -17,28 +18,8 @@ namespace OnlineShop.Data.Migrations
 
         protected override void Seed(OnlineShop.Data.OnlineShopDbContext context)
         {
-            var manager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new OnlineShopDbContext()));
-            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(new OnlineShopDbContext()));
-            var user = new ApplicationUser()
-            {
-                UserName = "cbui9",
-                Email = "thanhchinhbui@gmail.com",
-                EmailConfirmed = true,
-                BirthDay = DateTime.Now,
-                FullName = "Bui Thanh Chinh"
-            };
 
-            manager.Create(user, "123654$");
-
-            if (!roleManager.Roles.Any())
-            {
-                roleManager.Create(new IdentityRole { Name = "Admin" });
-                roleManager.Create(new IdentityRole { Name = "User" });
-            }
-
-            var adminUser = manager.FindByEmail("thanhchinhbui@gmail.com");
-            manager.AddToRoles(adminUser.Id, new string[] { "Admin", "User" });
-
+            CreateProductCategorySample(context);
             //  This method will be called after migrating to the latest version.
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
@@ -51,6 +32,49 @@ namespace OnlineShop.Data.Migrations
             //      new Person { FullName = "Rowan Miller" }
             //    );
             //
+        }
+        private void CreateUserSample(OnlineShop.Data.OnlineShopDbContext context)
+        {
+            var manager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new OnlineShopDbContext()));
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(new OnlineShopDbContext()));
+            if (context.Users.Count() == 0 && context.Roles.Count()==0)
+            {
+                var user = new ApplicationUser()
+                {
+                    UserName = "cbui9",
+                    Email = "thanhchinhbui@gmail.com",
+                    EmailConfirmed = true,
+                    BirthDay = DateTime.Now,
+                    FullName = "Bui Thanh Chinh"
+                };
+
+                manager.Create(user, "123654$");
+
+                if (!roleManager.Roles.Any())
+                {
+                    roleManager.Create(new IdentityRole { Name = "Admin" });
+                    roleManager.Create(new IdentityRole { Name = "User" });
+                }
+
+                var adminUser = manager.FindByEmail("thanhchinhbui@gmail.com");
+                manager.AddToRoles(adminUser.Id, new string[] { "Admin", "User" });
+            }
+        }
+
+        private void CreateProductCategorySample(OnlineShop.Data.OnlineShopDbContext context)
+        {
+            if (context.ProductCategories.Count() == 0)
+            {
+                List<ProductCategory> listProductCategory = new List<ProductCategory>()
+                {
+                    new ProductCategory() {Name = "Điện lạnh", Alias = "dien-lanh", Status = true },
+                    new ProductCategory() {Name = "Viễn thông", Alias = "vien-thong", Status = true },
+                    new ProductCategory() {Name = "Đồ gia dụng", Alias = "do-gia-dung", Status = true },
+                    new ProductCategory() {Name = "Mỹ phẩm", Alias = "my-pham", Status = true }
+                };
+                context.ProductCategories.AddRange(listProductCategory);
+                context.SaveChanges();
+            }
         }
     }
 }
